5ce9d1232185bb251d771a28dcc77129e9bc2a24
Kaushki Suryawanshi <kaushiki130613@gmail.com>
2026-07-09 23:35:53 +0530

    Fix Render Port

--- server/server.js
+++ server/server.js
@@ -55,8 +55,8 @@ app.delete("/api/notes/:id", (req, res) => {
   res.json({ message: "Note deleted" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
